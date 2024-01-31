919015870e0d83fb6b0e6dda922d3b03e9d0d66d
eldar <eldar00319@mail.ru>
2024-01-31 23:40:51 +0500

    Начальная версия приложения. Нет рекомендаций постов по эл почте

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class PublishedManager(models.Manager):
     #создали новый менеджер
@@ -13,11 +14,11 @@ class Post(models.Model):
         PUBLISHED = "PB", "Published"
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
-    #auto_now автоматическое заполнение времени при создании объекта
+    #auto_now_add автоматическое заполнение времени при создании объекта
     created = models.DateTimeField(auto_now_add=True)
     #auto_now автоматическое заполнение и обновление времени
     updated = models.DateTimeField(auto_now=True)
@@ -34,3 +35,10 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                                args=[self.publish.year,
+                                      self.publish.month,
+                                      self.publish.day,
+                                      self.slug])
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,50 @@
+import self
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
-def post_list(request):
-    posts = Post.published.all()
-    return render(request, 'blog/post/list.html', {'posts': posts})
+# def post_list(request):
+#     post_list = Post.published.all()
+#     #постраничная разбивка с 3 постами на страницу
+#     paginator = Paginator(post_list, 3)
+#     #параметр page содержит номер запрошенной страницы, если нет page, то возвращаем 1-ю страницу
+#     page_number = request.GET.get('page', 1)
+#     try:
+#         posts = paginator.page(page_number)
+#     except PageNotAnInteger:
+#         #если страница не целое число, то выдаем 1 стр
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#         #если номер страницы нах-ся вне диапозона, то выдаем посл стр
+#         posts = paginator.page(paginator.num_pages)
+#     return render(request, 'blog/post/list.html', {'posts': posts})
 
-def post_detail(request, id):
-    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
-    return render(request, 'blog/post/detail.html', {'post': post})
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 2
+    template_name = 'blog/post/list.html'
+
+# def post_detail(request, year, month, day, post):
+#     post = get_object_or_404(Post,
+#                              status=Post.Status.PUBLISHED,
+#                              slug=post,
+#                              publish__year=year,
+#                              publish__month=month,
+#                              publish__day=day)
+#     return render(request, 'blog/post/detail.html', {'post': post})
+
+class PostDetailView(DetailView):
+    model = Post
+    context_object_name = 'post'
+    template_name = 'blog/post/detail.html'
+
+    def get_object(self, queryset=None):
+        year = self.kwargs.get('year')
+        month = self.kwargs.get('month')
+        day = self.kwargs.get('day')
+        slug = self.kwargs.get('post')
+        post = get_object_or_404(Post, publish__year=year, publish__month=month, publish__day=day, slug=slug)
+        return post
